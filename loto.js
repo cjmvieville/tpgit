48d249113575bcf1d387b00f2d85da71abe7f9bf
Claude VIEVILLE <claude.vieville@univ-lille.fr>
2023-11-10 11:51:33 +0100

    Correction du fichier loto.js

--- loto.js
+++ loto.js
@@ -1,4 +1,16 @@
-int nbLignes=0:
-function incNbLignes(){
-	nbLignes++;*
+var nbLignes=0;
+var nbCartes=0;
+function incLigne(){
+	nbLignes++;
+}
+function decLigne(){
+	if(nbLigne>0) nbLignes--;
+}
+function incCarte(){
+	nbCartes++;
+}
+function decCarte(){
+	if(nbCartes>0) nbCartes--;
+}
+function selection(el){
 }
